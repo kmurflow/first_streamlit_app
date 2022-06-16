bd8a4e632e9ba91fcd334a6b4200d0da5183e0cd
kmurflow <57411001+kmurflow@users.noreply.github.com>
2022-06-16 15:29:45 -0500

    Update streamlit_app.py
    added fruit selection above where the fruit table is displayed

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#allows user to pick fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+
+#displays table on the page
 streamlit.dataframe(my_fruit_list)
+
